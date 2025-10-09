39175887f7faa1a3a9d1bc0c77c9905b3fbe3176
ArmAAC <armoop03@gmail.com>
2025-10-09 14:37:01 +0700

    [linkage Merg]

--- ServiceCatalog.Models/StoreGetLinkageDataModel.cs
+++ ServiceCatalog.Models/StoreGetLinkageDataModel.cs
@@ -11,7 +11,6 @@ namespace ServiceCatalog.Models
         public string Stkcode { get; set; }
         public int SeqLinkage { get; set; }
         public string KType { get; set; }
-        public string ProductId { get; set; }
         public string TruType { get; set; }
         public string Maker { get; set; }
         public string Model { get; set; }
